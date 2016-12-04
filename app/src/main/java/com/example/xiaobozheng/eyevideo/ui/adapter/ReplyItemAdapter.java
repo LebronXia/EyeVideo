8be7bc0671bb9967280eb472be3e5860c870740f
LebronXia <529330360@qq.com>
2016-12-05 01:12:56 +0800

    update

---
+++ app/src/main/java/com/example/xiaobozheng/eyevideo/ui/adapter/ReplyItemAdapter.java
@@ -0,0 +1,26 @@
+package com.example.xiaobozheng.eyevideo.ui.adapter;
+
+import android.content.Context;
+import android.view.ViewGroup;
+
+import com.example.xiaobozheng.eyevideo.model.RecycleViewItemData;
+import com.example.xiaobozheng.eyevideo.ui.view.recyclerview.adapter.BaseViewHolder;
+import com.example.xiaobozheng.eyevideo.ui.view.recyclerview.adapter.RecyclerArrayAdapter;
+
+import java.util.List;
+
+/**
+ * Created by Riane on 2016/12/5.
+ */
+
+public class ReplyItemAdapter extends RecyclerArrayAdapter<RecycleViewItemData> {
+
+    public ReplyItemAdapter(Context context) {
+        super(context);
+    }
+
+    @Override
+    public BaseViewHolder OnCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+}
